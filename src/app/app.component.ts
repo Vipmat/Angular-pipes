83effb043265939a5403bedd72af89b7b11dc9bf
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-09-30 18:55:02 +0530

    Impure and Async pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
+
   servers = [
     {
       instanceType: 'medium',
@@ -33,6 +39,8 @@ export class AppComponent {
     },
   ];
 
+  filteredStatus = '';
+
   getStatusClasses(server: {
     instanceType: string;
     name: string;
@@ -44,5 +52,13 @@ export class AppComponent {
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical',
     };
+  }
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017),
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ShortenPipe],
+  declarations: [AppComponent, ShortenPipe, FilterPipe],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
